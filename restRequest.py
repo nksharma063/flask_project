021504133abbfd42987c90a86c0be24cb8808848
Neeraj Sharma <nksharma063@gmail.com>
2023-07-20 15:09:00 +0530

    Trying to fetch the daya from documents through mongo db

---
+++ restRequest.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import json, os
+
+app = Flask(__name__)
+
+client = MongoClient(os.environ.get('MONGO_UR'))
+db = client.PythonAssignment
+
+database = db.get_collection('databaseCred')
+server = db.get_collection('serverCred')
+
+@app.route("/", methods = ["GET"])
+def get_PythonAssignment():
+    databaseDocs = database.find_one({'_id':False})
+    serverDocs = server.find_one({{'_id':False}})
+    return jsonify({'databaseDocs': databaseDocs, 'serverDocs':serverDocs})
+
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
+   
