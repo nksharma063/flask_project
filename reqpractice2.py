7a3ab1047be882500cdf1ab7f7e2a6b00e3fc72e
Neeraj Sharma <nksharma063@gmail.com>
2023-08-01 20:13:46 +0530

    added tthe login check as well from csv file

--- reqpractice2.py
+++ reqpractice2.py
@@ -19,6 +19,28 @@ def registration():
         writer.writerow([result['username'], result['password'], result['email']])
     return 'sucess'
 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        result = request.form
+        username = result['username']
+        password = result['password'] 
+        with open('abc.csv', 'r') as file:
+            reader = csv.reader(file)
+            # print(type(reader), reader)
+            for row in reader:
+                if len(row) < 3:
+                    continue
+                if row[0] == username:
+                    if row[1] == password:
+                        return "password also correct, login done"
+                    else:
+                        return "password incorrect try again"
+            return "username incorrect try again"                    
+            
+
+
+
 if __name__ == '__main__':
     app.run(port=3000, debug=True) 
 
